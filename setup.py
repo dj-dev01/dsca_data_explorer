6ef6f9268f9ae4db8895303d3f2a03c2fa1686f2
dj-dev01 <djx1337@gmail.com>
2025-05-26 22:31:58 -0800

    v0.3.1 Hotfix

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+APP = ['run_explorer.py']
+DATA_FILES = [
+    ('dsca_explorer/templates', ['dsca_explorer/templates/export_template.html']),
+    ('dsca_explorer/static', ['dsca_explorer/static/style.css'])
+]
+OPTIONS = {
+    'argv_emulation': True,
+    'plist': {
+        'CFBundleName': "DSCA Data Explorer",
+        'CFBundleShortVersionString': "1.0.0",
+        'NSHumanReadableCopyright': "Copyright © 2023, Your Name"
+    }
+}
 
 setup(
-    name="dsca-data-explorer",
-    version="1.0.0",
-    description="Multi-source geospatial/environmental data explorer for DSCA, emergency management, and research.",
-    author="DJ",
-    author_email="daltonn.ctr@palantir.com",
-    packages=find_packages(),
-    install_requires=[
-        "requests",
-        "pandas",
-        "openpyxl",
-        "python-docx",
-        "reportlab",
-        "urllib3"
-    ],
-    entry_points={
-        "console_scripts": [
-            "dsca-explorer = dsca_explorer.gui:run_gui"
-        ]
-    },
-    include_package_data=True,
-    license="MIT",
-    python_requires=">=3.8",
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
 )
